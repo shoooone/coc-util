aa55bbaa001d6c2e497bd08f4ca22189475137f4
shoooone <pride.a.s.w.12.01@gmail.com>
2020-06-17 13:58:54 +0900

    google anlytics導入

--- src/main.js
+++ src/main.js
@@ -1,12 +1,18 @@
-import Vue from "vue";
-import App from "./App.vue";
-import router from "./router";
-import vuetify from "./plugins/vuetify";
+import Vue from 'vue';
+import App from './App.vue';
+import router from './router';
+import vuetify from './plugins/vuetify';
+import VueAnalytics from 'vue-analytics';
 
 Vue.config.productionTip = false;
 
 new Vue({
   router,
   vuetify,
-  render: h => h(App)
-}).$mount("#app");
+  render: h => h(App),
+}).$mount('#app');
+
+Vue.use(VueAnalytics, {
+  id: 'UA-120234876-4',
+  router,
+});
